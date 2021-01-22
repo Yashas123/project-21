3809b21ff81ebf9ce8c5561b8a11af2592fadef0
Yashas123 <73638635+Yashas123@users.noreply.github.com>
2021-01-22 16:01:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var car,wall;
     createCanvas(1600, 400);
    speed=random(223,321);
     weight=random(30,52);
-    thickness=randome(22,83)
+    thickness=random(22,83)
    bullet=createSprite(50, 200, 50,5);
     bullet.velocityX = speed;
     bullet.shapeColor=color(255);
